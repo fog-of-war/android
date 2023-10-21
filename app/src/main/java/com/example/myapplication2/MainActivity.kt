cf671ff04ce44b668062cea14e2b8c1f7c4f5e22
yubin <shin.yubin18@gmail.com>
2023-10-21 14:16:27 +0900

    Feat : 위치 정보 허용 요청 코드 작성중
    https://github.com/leebyeungkok/AndroidHybridApp/blob/main/HybridNative/app/src/main/java/com/lbo/hybridnative/MainActivity.java

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -1,35 +1,37 @@
 package com.example.myapplication2
 
-import android.net.Uri
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationManager
 import android.net.http.SslError
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.SslErrorHandler
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabsIntent
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val mHandler = Handler(Looper.getMainLooper())
+    lateinit var mWebView: WebView
+    private var bCmdProcess = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /**크롬 맞춤탭 부분*/
-//        val url = "https://www.yubinhome.com"
-//        val intent = CustomTabsIntent.Builder()
-//            .setUrlBarHidingEnabled(true)
-//            .setShowTitle(true)
-//            .build();
-//
-//
-//        intent.launchUrl(this@MainActivity, Uri.parse(url))
-
-
-        val myWebView: WebView = findViewById(R.id.webView)
-
-        myWebView.settings.run {
+        /**웹뷰 부분*/
+        mWebView = findViewById(R.id.webView)
+        mWebView.settings.run {
             // 웹뷰 자바스크립트 허용
             javaScriptEnabled = true
             javaScriptCanOpenWindowsAutomatically = true
@@ -43,34 +45,86 @@ class MainActivity : AppCompatActivity() {
             userAgentString = "Chrome/56.0.0.0 Mobile"
         }
 
-        myWebView.webViewClient = WebViewClientClass()
-        myWebView.webChromeClient = WebChromeClient()
-        myWebView.loadUrl("https://www.yubinhome.com")
+        mWebView.webViewClient = WebViewClientClass()
+        mWebView.webChromeClient = WebChromeClient()
+        mWebView.loadUrl("https://www.yubinhome.com")
+
+        mWebView.addJavascriptInterface(NativeBridge(this), "Android")
     }
 
-    private fun openCustomTab(s: String) {
-
-    }
-
-    override fun onBackPressed() { // 뒤로가기 기능 구현
-        val myWebView: WebView = findViewById(R.id.webView)
-        if (myWebView.canGoBack()) {
-            myWebView.goBack()
+    /**뒤로가기*/
+    override fun onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack()
         } else {
             super.onBackPressed()
         }
     }
 
+    /**SSL 인증서 무시*/
     private inner class WebViewClientClass : WebViewClient() {
-        // SSL 인증서 무시
         override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
             handler?.proceed()
         }
-
-        // 페이지 내에서만 url 이동하게끔 만듬
         override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
             view?.loadUrl(url)
             return true
         }
     }
+
+    /**네이티브 브릿지*/
+    inner class NativeBridge(private val mActivity: Activity) {
+
+        @JavascriptInterface
+        fun callLocationPos(strCallbackFunc: String) {
+            if (ContextCompat.checkSelfPermission(mActivity, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mActivity, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            }
+            if (ContextCompat.checkSelfPermission(mActivity, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            }
+
+            Log.i("CALL NativeBridge callLocationPos", "START")
+            if (bCmdProcess) return
+
+            bCmdProcess = true
+            mHandler.post {
+                try {
+                    val locMgr = mActivity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    val criteria = Criteria()
+                    criteria.accuracy = Criteria.ACCURACY_COARSE
+                    val bestProv = locMgr.getBestProvider(criteria, true)
+                    val result1 = ActivityCompat.checkSelfPermission(mActivity, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    val result2 = ActivityCompat.checkSelfPermission(mActivity, android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+                    if (result1 != PackageManager.PERMISSION_GRANTED && result2 != PackageManager.PERMISSION_GRANTED) {
+                        return@post
+                    }
+
+                    val loc: Location = locMgr.getLastKnownLocation(bestProv.toString()) ?: return@post
+                    val strJavascript = "$strCallbackFunc('${loc.latitude}','${loc.longitude}')"
+                    Log.e("", strJavascript)
+                    mWebView.loadUrl("javascript:$strJavascript")
+                } catch (exLoc: Exception) {
+                    val strJavascript = "alert('위치확인오류')"
+                    mWebView.loadUrl("javascript:$strJavascript")
+                }
+                bCmdProcess = false
+            }
+        }
+    }
 }
+
+/**크롬 맞춤탭 부분*/
+//        val url = "https://www.yubinhome.com"
+//        val intent = CustomTabsIntent.Builder()
+//            .setUrlBarHidingEnabled(true)
+//            .setShowTitle(true)
+//            .build();
+//
+//
+//        intent.launchUrl(this@MainActivity, Uri.parse(url)) // //        private fun openCustomTab(s: String) {
+//
+//     }
