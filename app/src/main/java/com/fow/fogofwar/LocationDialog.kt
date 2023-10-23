b7e9da9007b7c5ea4afbe32a7c450b72392959ae
yubin <shin.yubin18@gmail.com>
2023-10-23 11:32:35 +0900

    Feat : 구글 플레이 위치 서비스 설치

---
+++ app/src/main/java/com/fow/fogofwar/LocationDialog.kt
@@ -0,0 +1,14 @@
+package com.fow.fogofwar
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+
+class LocationDialog(context: Context): Dialog(context) {
+    val TAG:String = "로그"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
